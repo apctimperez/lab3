6f787aa2ca4ca7b31ee2a16663270e64f571df09
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2024-02-20 14:55:25 +0800

    edited file

--- ci4/app/Views/pages/selectdata.php
+++ ci4/app/Views/pages/selectdata.php
@@ -1,15 +1,15 @@
 <?php
 // For Xampp Localhost
-//$servername = "localhost";
-//$username = "root";
-//$password = "";
-//$dbname = "myDB";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
  
 //For Socitcloud
-$servername = "localhost";
-$username = "webprogmi221";
-$password = "g_6bCitLu.ljMK*m";
-$dbname = "webprogmi221";
+//$servername = "localhost";
+//$username = "webprogmi221";
+//$password = "g_6bCitLu.ljMK*m";
+//$dbname = "webprogmi221";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error) {
