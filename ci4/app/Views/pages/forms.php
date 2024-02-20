c0704cd2b5ce75f80f4f3cb285b9bf24326777e8
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2024-02-20 21:12:24 +0800

    edited files

--- ci4/app/Views/pages/forms.php
+++ ci4/app/Views/pages/forms.php
@@ -2192,7 +2192,7 @@ if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
  
-$sql = "INSERT INTO MyGuests (name, email, website, comment, gender)
+$sql = "INSERT INTO trperez_myguests (name, email, website, comment, gender)
 VALUES ('$name', '$email', '$website', '$comment', '$gender')";
  
  
