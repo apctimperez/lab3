709abbbacf6fac40faf7035561f7ecc28350788a
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2024-02-20 21:11:06 +0800

    edited files

--- ci4/app/Views/pages/selectdata.php
+++ ci4/app/Views/pages/selectdata.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM jmmiyabe_myguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM trperez_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
