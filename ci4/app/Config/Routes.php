0f05f5559cf364bcb15f096c7db458fbad27d7ac
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2024-03-06 15:32:47 +0800

    Added Guests

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -13,6 +13,8 @@ use App\Controllers\News; // Add this line
 use App\Controllers\Pages;
 
 $routes->get('guests', [Guests::class, 'index']);           // Add this line
+$routes->get('guests/(:segment)', [Guests::class, 'guest']);
+$routes->post('guests', [Guests::class, 'create']); // Add this line
 $routes->get('guests/(:segment)', [Guests::class, 'show']);
 
 $routes->get('news', [News::class, 'index']);
